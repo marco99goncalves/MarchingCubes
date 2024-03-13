96622a801ef554c1293e4b5fb217411bd8a827fc
Hugo Cardante <hugocardante@gmail.com>
2024-03-13 23:11:55 +0000

    Added the first music visuals, looking sick

--- Assets/PointController.cs
+++ Assets/PointController.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
-using static Unity.Mathematics.math;
-using Unity.Mathematics;
-using System.Threading;
 
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+
+
+[RequireComponent(typeof(AudioSource))]
 public class PointController : MonoBehaviour
 {
     public int numberOfPoints;
@@ -21,50 +18,121 @@ public class PointController : MonoBehaviour
     int[,] points;
     public float simulationTime;
     public float timer;
-    public float xInc = 0;
-    public float yInc = 0;
-    public float zInc = 0;
-    private float zoff = 0;
-    public float INC_VALUE;
+    //public float xInc = 0;
+    //public float yInc = 0;
+
+    //public float zInc = 0;
+
+    //private float zoff = 0;
+    //public float INC_VALUE;
+
+    private AudioSource _audioSource;
+    public float[] _samples;
 
     // Start is called before the first frame update
     void Start()
     {
         //Random.InitState(42);
+        Application.targetFrameRate = 120;
         InitializePoints();
+        _samples = new float[numberOfPoints * numberOfPoints];
+        _audioSource = GetComponent<AudioSource>();
         timer = simulationTime;
     }
 
     void InitializePoints()
     {
         points = new int[numberOfPoints, numberOfPoints];
-        for(int x = 0; x < numberOfPoints; x++)
+        for (int x = 0; x < numberOfPoints; x++)
         {
-            for(int y = 0; y < numberOfPoints; y++)
+            for (int y = 0; y < numberOfPoints; y++)
             {
-                GameObject obj = Instantiate(pointTemplate, new Vector3(x*res, y*res, 0), Quaternion.identity);
+                GameObject obj = Instantiate(pointTemplate, new Vector3(x * res, y * res, 0), Quaternion.identity);
             }
         }
     }
 
     void MarchTheSquares()
     {
-        float xoff = 0;
-        for (int x = 0; x < numberOfPoints; x++)
+        GetSpectrumAudioSource();
+      
+        var curMin = float.MaxValue;
+        var curMax = float.MinValue;
+        for (var x = 0; x < numberOfPoints; ++x)
         {
-            xoff += xInc;
-            float yoff = 0;
-            for (int y = 0; y < numberOfPoints; y++)
-            {
-                //float sX = (float)(x + xInc) / long.MaxValue;
-                //float sY = (float)(y + yInc) / long.MaxValue;
-
-                float val = noise.snoise(new float3(xoff, yoff, zoff));
-                points[x, y] = val*Mathf.Cos(Time.deltaTime) > 0.2f*Mathf.Sin(Time.deltaTime) ? 1 : 0;
-                yoff += yInc;
-            }
+            curMin = min(curMin, _samples[x]);
+            curMax = max(curMax, _samples[x]);
         }
-        zoff += zInc;
+
+
+        int size = numberOfPoints; // Assume numberOfPoints is defined somewhere
+        var elements = size * size;
+        int[][] points = new int[size][]; // Assume points is defined somewhere
+        for (int index = 0; index < size; index++)
+        {
+            points[index] = new int[size];
+        }
+
+        int myRow, myCol;
+        if (size % 2 == 0)
+        {
+            myRow = size / 2 - 1;
+            myCol = size / 2;
+        }
+        else
+        {
+            myRow = myCol = size / 2;
+        }
+
+        int steps = 1; 
+        bool increaseSteps = false;
+        int direction = 0;
+        int count = 0;
+
+       
+        float curMatVal = _samples[count++];
+        float normalized = ((curMatVal - curMin) / (curMax - curMin));
+        points[myRow][myCol] = (30 * normalized >= 0.03f ? 0 : 1);
+
+        while (count < elements)
+        {
+            for (int step = 0; step < steps; step++)
+            {
+                switch (direction)
+                {
+                    case 0:
+                        myCol++;
+                        break; // Move right
+                    case 1:
+                        myRow++;
+                        break; // Move down
+                    case 2:
+                        myCol--;
+                        break; // Move left
+                    case 3:
+                        myRow--;
+                        break; // Move up
+                }
+
+                // Check boundaries
+                if (myRow < 0 || myRow >= size || myCol < 0 || myCol >= size) continue;
+
+                // Place element
+                curMatVal = _samples[count++];
+                normalized = ((curMatVal - curMin) / (curMax - curMin));
+                points[myRow][myCol] = (30 * normalized >= 0.03f ? 0 : 1);
+
+                if (count >= elements) break;
+            }
+
+            if (increaseSteps) steps++; // Increase steps every other direction change
+            increaseSteps = !increaseSteps; // Toggle flag every direction
+
+            direction = (direction + 1) % 4; // Cycle through directions
+        }
+
+
+
 
         for (int x = 0; x < numberOfPoints - 1; x++)
         {
@@ -76,7 +144,8 @@ public class PointController : MonoBehaviour
                 Vector3 c = new Vector3(x * res + half, y * res + res, 0);
                 Vector3 d = new Vector3(x * res, y * res + half, 0);
 
-                int currentCase = points[x, y] * 1 + points[x + 1, y] * 2 + points[x + 1, y + 1] * 4 + points[x, y + 1] * 8;
+                int currentCase = points[x][y] * 1 + points[x + 1][y] * 2 + points[x + 1][y + 1] * 4 +
+                                  points[x][y + 1] * 8;
                 DrawCase(a, b, c, d, currentCase);
             }
         }
@@ -133,18 +202,25 @@ public class PointController : MonoBehaviour
         }
     }
 
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+
     void DrawLine(Vector3 point1, Vector3 point2)
     {
         GameObject lineHolder = new GameObject();
-        lineHolder.transform.parent = transform; 
+        lineHolder.transform.parent = transform;
 
         LineRenderer lineRenderer = lineHolder.AddComponent<LineRenderer>();
         lineRenderer.positionCount = 2; // Set the number of points in the line
-        lineRenderer.startWidth = res/10;
-        lineRenderer.endWidth = res/10;
+        lineRenderer.startWidth = res / 10;
+        lineRenderer.endWidth = res / 10;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.white;
-        lineRenderer.endColor = Color.white;
+        lineRenderer.startColor = new UnityEngine.Color(1, 1, 1);
+        lineRenderer.endColor = new UnityEngine.Color(1, 1, 1);
+
 
         // Set the start and end points of the line
         lineRenderer.SetPosition(0, point1); // Start point
@@ -158,6 +234,7 @@ public class PointController : MonoBehaviour
         {
             Destroy(line);
         }
+
         linesCreated.Clear();
     }
 
@@ -167,20 +244,21 @@ public class PointController : MonoBehaviour
         {
             Destroy(point);
         }
+
         pointsCreated.Clear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //timer -= Time.deltaTime;
-        if(timer <= 0)
+        timer -= Time.deltaTime;
+        if (timer <= 0)
         {
             RunSimulation = true;
             timer = simulationTime;
         }
 
-        if(RenderLines)
+        if (RenderLines)
         {
             DestroyPoints();
             DestroyLines();
